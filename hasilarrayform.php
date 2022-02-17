8c62d5f9d54a7d69931dd7dfb6bacf94247e8b09
Kizmine <97926227+berli122@users.noreply.github.com>
2022-02-17 11:44:13 +0700

    Tugas Form

---
+++ hasilarrayform.php
@@ -0,0 +1,83 @@
+<?php
+
+if (isset($_POST['simpan'])) {
+    $nis = $_POST['nis'];
+    $nama = $_POST['nama'];
+    $kelas = $_POST['kelas'];
+    $bind = $_POST['bind'];
+    $bing = $_POST['bing'];
+    $mtk = $_POST['mtk'];
+    $prd = $_POST['prd'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Siswa</title>
+</head>
+<body>
+    <center>
+        <h2>Data Siswa Kelas XI RPL 1</h2>
+        <table border="1" cellspacing="0">
+            <tr>
+                <th>Nis</th>
+                <th>Nama</th>
+                <th>Kelas</th>
+                <th>Bahasa Indonesia</th>
+                <th>Bahasa Inggris</th>
+                <th>Matematika</th>
+                <th>Produktif</th>
+                <th>Total Nilai</th>
+                <th>Rata - rata</th>
+                <th>Grade</th>
+            </tr>
+            <?php
+
+for ($a = 0; $a < count($nis); $a++) {
+
+    ?>
+            <tr>
+                <td><?php echo $nis[$a]; ?></td>
+                <td><?php echo $nama[$a]; ?></td>
+                <td><?php echo $kelas[$a]; ?></td>
+                <td><?php echo $bind[$a]; ?></td>
+                <td><?php echo $bing[$a]; ?></td>
+                <td><?php echo $mtk[$a]; ?></td>
+                <td><?php echo $prd[$a];?></td>
+                <td><?php echo
+                    $nilai = $bind[$a] + $bing[$a] + $mtk[$a] + $prd[$a];
+                    $nilai;
+                    ?>
+                </td>
+                <td><?php echo $grade = $nilai/4;
+                            $grade;
+                            ?></td>
+                <td><?php if ($grade >= 90){
+                   echo "A";
+                }else if($grade >= 70){
+                   echo "B";
+                }else if($grade >= 50) {
+                    echo "C";
+                } else if ($grade <= 50) {
+                    echo "D";
+                } 
+            }
+                
+                ?></td>
+            </tr>
+            
+
+            
+            </tr>
+
+
+
+        </table>
+    </center>
+</body>
+</html>
